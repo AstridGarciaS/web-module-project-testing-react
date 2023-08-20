ee10cd6cc0d49120174d4190418e521695d64318
AstridGarciaS <119351821+AstridGarciaS@users.noreply.github.com>
2023-08-20 00:06:10 -0400

    update

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -38,7 +38,7 @@ test("renders the summary test passed as prop", () => {
 
 test("renders default image when image is not defined", () => {
     render(<Episode episode={testEpisodeWithoutImage}/>);
-    const image = screen.queryAllByText('./stranger_things.png');
+    const image = screen.queryByAltText('./stranger_things.png');
     expect(image).toBeInTheDocument();
 
 });
